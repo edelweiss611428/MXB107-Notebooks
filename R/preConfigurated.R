f7652ff56d0e0c88845a63d1c1995df40deac9b4
edelweiss611428 <edelweiss611428@gmail.com>
2025-07-16 10:40:00 +1000

    Update preConfigurated.R

--- R/preConfigurated.R
+++ R/preConfigurated.R
@@ -1,4 +1,5 @@
 if (!require("ggplot2")) install.packages("ggplot2", dependencies = T); library("ggplot2") ##visualisation
 if (!require("dplyr")) install.packages("dplyr", dependencies = T); library("dplyr") ##wrangling
 if (!require("magrittr")) install.packages("magrittr", dependencies = T); library("magrittr") ##pipeline
-if (!require("e1071")) install.packages("e1071", dependencies = T); library("e1071") ##skewness
+
+source("./R/utils.R")
