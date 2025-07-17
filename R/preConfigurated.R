de6e61d9f3cde787db50727c777cec14db853d65
edelweiss611428 <edelweiss611428@gmail.com>
2025-07-17 11:11:02 +1000

    Update preConfigurated.R

--- R/preConfigurated.R
+++ R/preConfigurated.R
@@ -1,5 +1,6 @@
 if (!require("ggplot2")) install.packages("ggplot2", dependencies = T); library("ggplot2") ##visualisation
 if (!require("dplyr")) install.packages("dplyr", dependencies = T); library("dplyr") ##wrangling
+if (!require("tidyr")) install.packages("tidyr", dependencies = T); library("tidyr") ##wrangling
 if (!require("stringr")) install.packages("stringr", dependencies = T); library("stringr") ##wrangling
 if (!require("magrittr")) install.packages("magrittr", dependencies = T); library("magrittr") ##pipeline
 
